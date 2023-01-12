3ba2329118e7f8aed47309e305cdf886e3d3270d
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-01-12 15:40:14 -0500

    deconnexion en commentaire car non fonctionnel

--- vues/entete.php
+++ vues/entete.php
@@ -13,8 +13,9 @@
 	<link rel="stylesheet" href="./css/main.css">
 
 	<script src="./js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-		 <script src="./js/plugins.js"></script>
-		<script src="./js/main.js"></script>
+	<script src="./js/plugins.js"></script>
+	<script src="./js/main.js"></script>
+	
 </head>
 
 <body>
